191203fbd04e36c4510d537e7177a55678d82a78
Revy Pramana <111827245+Revprm@users.noreply.github.com>
2023-04-17 05:50:21 +0700

    Create LYN2.cpp

---
+++ Practicum 2/LYN2.cpp
@@ -0,0 +1,204 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+int arr1[2000], arr2[2000], hght = 0;
+
+typedef struct bstnode_t {
+    int key;
+    struct bstnode_t *left, *right;
+} BSTNode;
+
+typedef struct bst_t {
+    BSTNode *_root;
+    unsigned int _size;
+} BST;
+
+/**
+ * !!! WARNING UTILITY FUNCTION !!!
+ * Recognized by prefix "__bst__"
+ * ---------------------------------------------
+ * Note that you better never access these functions, 
+ * unless you need to modify or you know how these functions work.
+ */
+
+BSTNode* __bst__createNode(int value) {
+    BSTNode *newNode = (BSTNode*) malloc(sizeof(BSTNode));
+    newNode->key = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+BSTNode* __bst__insert(BSTNode *root, int value) {
+    if (root == NULL) 
+        return __bst__createNode(value);
+
+    if (value < root->key)
+        root->left = __bst__insert(root->left, value);
+    else if (value > root->key)
+        root->right = __bst__insert(root->right, value);
+    
+    return root;
+}
+
+BSTNode* __bst__search(BSTNode *root, int value) {
+    while (root != NULL) {
+        if (value < root->key)
+            root = root->left;
+        else if (value > root->key)
+            root = root->right;
+        else
+            return root;
+    }
+    return root;
+}
+
+BSTNode* __bst__findarr1(BSTNode *node) {
+    BSTNode *currNode = node;
+    while (currNode && currNode->left != NULL)
+        currNode = currNode->left;
+    
+    return currNode;
+}
+
+BSTNode* __bst__remove(BSTNode *root, int value) {
+    if (root == NULL) return NULL;
+
+    if (value > root->key) 
+        root->right = __bst__remove(root->right, value);
+    else if (value < root->key) 
+        root->left = __bst__remove(root->left, value);
+    else {
+        
+        if (root->left == NULL) {
+            BSTNode *rightChild = root->right;
+            free(root);
+            return rightChild;
+        }
+        else if (root->right == NULL) {
+            BSTNode *leftChild = root->left;
+            free(root);
+            return leftChild;
+        }
+
+        BSTNode *temp = __bst__findarr1(root->right);
+        root->key     = temp->key;
+        root->right   = __bst__remove(root->right, temp->key);
+    }
+    return root;
+}
+
+void __bst__inorder(BSTNode *root) {
+    if (root) {
+        __bst__inorder(root->left);
+        printf("%d ", root->key);
+        __bst__inorder(root->right);
+    }
+}
+
+void __bst__postorder(BSTNode *root) {
+    if (root) {
+        __bst__postorder(root->left);
+        __bst__postorder(root->right);
+        printf("%d ", root->key);
+    }
+}
+
+void __bst__preorder(BSTNode *root) {
+    if (root) {
+        printf("%d ", root->key);
+        __bst__preorder(root->left);
+        __bst__preorder(root->right);
+    }
+
+}
+void __extremeTraverse(BSTNode *root, int level) {
+    if (!root) return;
+    if (root->key < arr1[level]) arr1[level] = root->key;
+    if (root->key > arr2[level]) arr2[level] = root->key; 
+    if (hght < level) hght = level;
+    __extremeTraverse(root->left, level + 1);
+    __extremeTraverse(root->right, level + 1);
+}
+/**
+ * PRIMARY FUNCTION
+ * ---------------------------
+ * Accessible and safe to use.
+ */
+
+void bst_init(BST *bst) {
+    bst->_root = NULL;
+    bst->_size = 0u;
+}
+
+bool bst_isEmpty(BST *bst) {
+    return bst->_root == NULL;
+}
+
+bool bst_find(BST *bst, int value) {
+    BSTNode *temp = __bst__search(bst->_root, value);
+    if (temp == NULL)
+        return false;
+    
+    if (temp->key == value)
+        return true;
+    else
+        return false;
+}
+
+void bst_insert(BST *bst, int value) {
+    if (!bst_find(bst, value)) {
+        bst->_root = __bst__insert(bst->_root, value);
+        bst->_size++;
+    }
+}
+
+void bst_remove(BST *bst, int value) {
+    if (bst_find(bst, value)) {
+        bst->_root = __bst__remove(bst->_root, value);
+        bst->_size--;
+    }
+}
+
+/**
+ * Binary search tree traversal
+ * - Inorder
+ * - Postorder
+ * - Preorder
+ */
+
+void bst_inorder(BST *bst) {
+    __bst__inorder(bst->_root);
+}
+
+void bst_postorder(BST *bst) {
+    __bst__postorder(bst->_root);
+}
+
+void bst_preorder(BST *bst) {
+    __bst__preorder(bst->_root);
+}
+
+void extremeNodes(BSTNode *root){
+    __extremeTraverse(root, 0);
+}
+
+int main(){
+    BST set;
+    bst_init(&set);
+    memset(arr1, 5, sizeof(arr1));
+    memset(arr2, 0, sizeof(arr2));
+    int n, num; cin >> n;
+    while (n--){
+        cin >> num; bst_insert(&set, num);
+    }
+    extremeNodes(set._root);
+    int chk = 1;
+    for (int i = hght; i >= 0; i--) {
+        if (chk) cout << arr1[i] << " ";
+        else cout << arr2[i] << " ";
+        chk = !chk;
+    }
+    
+    return 0;
+}
